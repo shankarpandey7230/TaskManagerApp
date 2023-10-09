1946acd06d4a4aaf63370bc61e99f8932409e79d
shankarpandey7230 <shankarpandey7230@gmail.com>
2023-10-09 11:19:13 +1100

    updating tasks

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -30,9 +30,6 @@ const getTask = async (req, res) => {
   }
 };
 
-const updateTask = (req, res) => {
-  res.send("update Tasks");
-};
 const deleteTask = async (req, res) => {
   try {
     const { id: taskID } = req.params;
@@ -41,6 +38,22 @@ const deleteTask = async (req, res) => {
       return res.status(404).json({ msg: `No Task with id :${taskID}` });
     }
     res.status(200).json({ task });
+  } catch (error) {
+    res.status(500).json({ msg: error });
+  }
+};
+const updateTask = async (req, res) => {
+  try {
+    const { id: taskID } = req.params;
+    const task = await Task.findOneAndUpdate({ _id: taskID }, req.body, {
+      new: true,
+      runValidators: true,
+    });
+    if (!task) {
+      return res.status(404).json({ msg: `No Task with id :${taskID}` });
+    }
+
+    res.status(200).json({});
   } catch (error) {
     res.status(500).json({ msg: error });
   }
